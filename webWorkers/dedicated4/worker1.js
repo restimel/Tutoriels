2246d0537c7a660d3b9ca5810c0bc89b92f5b5ee
Restimel <restimel@gmail.com>
2011-11-19 15:30:54 +0100

    webWorkers - dedicated4: creation

---
+++ webWorkers/dedicated4/worker1.js
@@ -0,0 +1,19 @@
+if(self.Worker){
+	//on crée un nouveau worker
+	var w=new Worker("worker2.js");
+	w.addEventListener("message",function(event){
+		//on transmet au parent (en modifiant le message)
+		postMessage('worker2 me dit de te dire que "'+event.data+'"');
+	},false);
+
+	//on gère la communication avec le parent
+	addEventListener("message",function(event){
+		//on transmet le message au worker tel quel
+		w.postMessage(event.data);
+	},false);
+
+}else{
+  //le navigateur ne gère pas les workers dans un worker
+  postMessage("Votre navigateur ne supporte pas les Workers dans les Workers ?");
+}
+
---
+++ webWorkers/dedicated4/worker2.js
@@ -0,0 +1,4 @@
+addEventListener("message",function(event){
+	postMessage("C'est sympa de m'écrire");
+},false);
+
