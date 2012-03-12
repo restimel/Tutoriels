16c1a67972e463f9792270bbda3ab6e4a5b54597
Restimel <restimel@gmail.com>
2012-03-12 23:20:34 +0100

    changement d'algo pour Alpha Beta

--- webWorkers/TP_morpion/sansWorker/IA.js
+++ webWorkers/TP_morpion/sansWorker/IA.js
@@ -1,64 +1,71 @@
 var profondeurMax = 4; //indique la profondeur de recherche de l'IA
 
 //demande à l'IA de jouer
-function iaJoue(grilleOrig,couleur,profondeur){
-	profondeur = profondeur || 0;
-	if(profondeur>=profondeurMax){
+function iaJoue(grilleOrig,couleur){
+	var grille = copieGrille(grilleOrig);
+	return iaAlphaBeta(grille, couleur, profondeurMax, -Infinity, Infinity);
+}
+
+//fonction alphabeta
+function iaAlphaBeta(grille, couleur, profondeur, alpha, beta){
+	if(!profondeur){
 		//on a atteint la limite de profondeur de calcul
-		return estimation(grilleOrig);
+		return iaEstimation(grille);
 	}else{
-		var grille = copieGrille(grilleOrig);
-		var minMax = Infinity;
-		var fCompare = Math.min;
-		var couleurOpp = couleur%2+1;
-		var coup=[];
+		var meilleur = -Infinity;
 		var estim;
-		if(couleur===1){
-			minMax = -Infinity;
-			fCompare = Math.max;
-		}
+		var coup=null;
+		var couleurOpp = couleur%2+1;
 		for(var x=0;x<nx;x++){
 			for(var y=0;y<ny;y++){
 				if(grille[x][y]) continue; //case déjà occupée
+				
+				if(!coup){coup=[x,y];} //pour proposer au moins un coup
+				
 				grille[x][y]=couleur; //on va essayer avec ce coup
 				//vérifie si le coup est gagnant
 				if(estim=verifVainqueur(x,y,grille)){
-					if(profondeur===0){
+					grille[x][y]=0; //restauration de la grille
+					if(profondeur===profondeurMax){
 						return [x,y];
 					}else{
-						return estim===1?Infinity:-Infinity;
+						return Infinity;
 					}
 				}
-				estim=iaJoue(grille,couleurOpp,profondeur+1); //on regardes les possibilités adverses
-				if((couleur===1 && estim===Infinity) || (couleur===0 && estim===-Infinity)){
-				//coup qui gagne
-					if(profondeur===0){
-						return [x,y];
-					}else{
-						return estim;
+				estim = -iaAlphaBeta(grille, couleurOpp, profondeur-1, -beta, -alpha);
+			
+				if(estim > meilleur){
+					//on vient de trouver un meilleur coup
+					meilleur = estim;
+					if(meilleur > alpha){
+						alpha = meilleur;
+						coup = [x,y];
+						if(alpha >= beta){
+							grille[x][y]=0; //restauration de la grille
+							if(profondeur===profondeurMax){
+								return coup;
+							}else{
+								return meilleur;
+							}
+						}
 					}
 				}
-				minMax=fCompare(estim,minMax);
-				if(profondeur===0 && minMax===estim){
-					//on a trouvé un nouveau meilleur coup
-					coup = [x,y];
-				}
-				
-				//restauration de la grille
-				grille[x][y]=0;
+				grille[x][y]=0; //restauration de la grille
 			}
 		}
-		if(profondeur===0){
+		if(profondeur===profondeurMax){
 			return coup;
 		}else{
-			return minMax;
+			if(coup) return meilleur;
 		}
 	}
-	
 }
 
+
+
 //permet d'estimer la position
-function estimation(grille){
+function iaEstimation(grille){
+	//TODO
 	return 0;
 }
 
@@ -66,10 +73,7 @@ function estimation(grille){
 function copieGrille(grille){
 	var nvGrille=[];
 	for(var x=0;x<nx;x++){
-		nvGrille[x]=[];
-		for(var y=0;y<ny;y++){
-			nvGrille[x][y]=grille[x][y];
-		}
+		nvGrille[x]=grille[x].concat([]);//effectue une copie de la liste
 	}
 	return nvGrille;
 }
