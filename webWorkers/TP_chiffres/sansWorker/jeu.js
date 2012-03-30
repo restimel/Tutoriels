33e451ff393d6273d06279d087e154b223d8d0c9
Restimel <restimel@gmail.com>
2012-03-30 22:59:13 +0200

    creation TP chiffre (sans worker)

---
+++ webWorkers/TP_chiffres/sansWorker/jeu.js
@@ -0,0 +1,241 @@
+/*
+  Références aux éléments HTML
+*/
+
+var zoneJeu = document.getElementById("zoneJeu");
+var zonePlaque = document.getElementById("zonePlaque");
+var zoneParametre = document.getElementById("zoneParametres");
+var zoneResulat = document.getElementById("zoneResultat");
+
+var nombresDispo = document.getElementById("nombresDispo");
+var zoneCalcul = document.getElementById("zoneCalcul");
+
+var regleNbPlaques = document.getElementById("regleNbPlaques");
+var regleTemps = document.getElementById("regleTemps");
+
+var jeuCible = document.getElementById("jeuCible");
+var jeuTemps = document.getElementById("jeuTemps");
+var jeuDistance = document.getElementById("jeuDistance");
+
+var inputFormule = document.getElementById("entreeFormule");
+
+var listeNombre = [];
+
+//paramétrage par défaut
+regleNbPlaques.value=6;
+regleTemps.value=45;
+document.getElementById("boutonCommence").onclick=initialisation;
+
+
+
+// initialisation d'une nouvelle partie
+function initialisation(){
+	zoneParametre.style.display = "none";
+	zoneJeu.style.display = "block";
+	jeuCible.value = "???";
+	jeuTemps.value = regleTemps.value;
+	
+	zonePlaque.innerHTML = "";
+	nombresDispo.innerHTML = "";
+	zoneCalcul.innerHTML = "";
+	
+	listeNombre = [];
+	
+	generateNombre();
+	
+	inputFormule.addEventListener("blur",restoreFocus,false);
+	inputFormule.addEventListener("keypress",analyseFormule,false);
+	inputFormule.addEventListener("blur",analyseFormule,false);
+	inputFormule.focus();
+}
+
+//gestion du chronometre
+var chronometre=(function(){
+	var timer,init;
+	function chrono(){
+		var temps = (Date.now() - init)/1000;
+		jeuTemps.value = Math.round(regleTemps.value - temps);
+		if(temps>regleTemps.value){
+			clearInterval(timer);
+			//TODO fin
+		}
+	}
+	
+	return function(){
+		init = Date.now();
+		timer = setInterval(chrono,400);
+	};
+})();
+
+//permet de générer les nombres pour jouer et défini la cible
+function generateNombre(){
+	var choix = [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,25,50,75,100];
+	if(listeNombre.length < regleNbPlaques.value){
+		listeNombre.push(new Nombre(null,null,null,choix[Math.floor(Math.random()*choix.length)]));
+		setTimeout(generateNombre,500);
+	}else{
+		jeuCible.value = Math.floor(Math.random()*898)+1;
+		chronometre();
+	}
+}
+
+//permet de redonner le focus à l'input quand il le perd
+function restoreFocus(event){
+	setTimeout(function(){event.target.focus();},20);
+}
+
+//permet d'analyser l'entrée de l'utilisateur
+function analyseFormule(event){
+	var key = event.keyCode || event.which;
+	if(key === undefined || key === 13 || key === 61 || key === 9){
+		var operation = this.value.match(/\s*(\d+)\s*([-+*_\\/÷&xX×])\s*(\d+)/);
+		if(operation){
+			var n1 = listeNombre.filter(function(el){return !el.usedBy && el.valeur == operation[1]})[0],
+			    n2 = listeNombre.filter(function(el){return !el.usedBy && el.valeur == operation[3] && el !== n1})[0];
+			switch(operation[2]){
+				case "&":
+				case "+":
+					operation = "+";
+				break;
+				case "_":
+				case "-":
+					operation = "-";
+				break;
+				case "*":
+				case "x":
+				case "X":
+				case "×":
+					operation = "×";
+				break;
+				case "/":
+				case "\\":
+				case "÷":
+					operation = "÷";
+				break;
+				default:
+					operation = null;
+			}
+		}
+		if(operation && n1 && n2){
+			var n = new Nombre(n1,n2,operation);
+			if(n.valeur){
+				listeNombre.push(n);
+				this.value = "";
+				majDistance();
+			}
+		}else{
+			this.className = "erreur";
+		}
+	}else{
+		this.className = "";
+		if(key === 27){
+			this.value = "";
+		}
+	}
+}
+
+//met à jour la distance entre les nombres et la cible
+function majDistance(){
+	var distance = Infinity;
+	var cible = jeuCible.value;
+	listeNombre.forEach(function(el){
+		distance = Math.min(distance,Math.abs(el.valeur-cible));
+	});
+	if(distance){
+		jeuDistance.value = "Compte approchant : " + distance;
+	}else{
+		jeuDistance.value = "Le compte est bon !";
+	}
+}
+
+//création et affichage d'une plaque
+function creationPlaque(nb){
+	var plaque = document.createElement("div");
+	plaque.textContent = nb;
+	zonePlaque.appendChild(plaque);
+	return plaque;
+}
+
+//constructeur d'un objet représentant les nombres manipulés par l'utilisateur
+function Nombre(parent1,parent2,op,init){
+	this.parent1 = parent1;
+	this.parent2 = parent2;
+	this.operateur = op;
+	this.usedBy = null;
+	
+	if(init){
+		this.valeur = init;
+		creationPlaque(init);
+	}else{
+		switch(op){
+			case "+":
+				this.valeur = parent1.valeur + parent2.valeur;
+			break;
+			case "-":
+				this.valeur = parent1.valeur - parent2.valeur;
+			break;
+			case "×":
+				this.valeur = parent1.valeur * parent2.valeur;
+			break;
+			case "÷":
+				this.valeur = parent1.valeur / parent2.valeur;
+			break;
+		}
+		if(this.valeur < 0 || this.valeur !== Math.round(this.valeur)){
+			this.valeur = 0;
+			return null;
+		}
+		this.parent1.utilise(this);
+		this.parent2.utilise(this);
+		this.createCalcul();
+	}
+	this.refDispo = creationNombresDispo(this.valeur);
+	this.refDispo.className = "nombreDispo";
+	
+	
+}
+
+Nombre.prototype.createCalcul = function(){
+	this.refCalcul = document.createElement("div");
+	this.refCalcul.textContent = this.parent1.valeur + " " + this.operateur + " " + this.parent2.valeur + " = " + this.valeur;
+	if(this.valeur == jeuCible.value){
+		this.refCalcul.className = "compteBon";
+	}else{
+		var that = this;
+		this.refCalcul.addEventListener("click",function(){that.supprime();},false);
+	}
+	zoneCalcul.appendChild(this.refCalcul);
+};
+
+Nombre.prototype.utilise = function(parent){
+	this.refDispo.className = "nombreNonDispo";
+	this.usedBy = parent;
+};
+
+Nombre.prototype.libre = function(){
+	this.refDispo.className = "nombreDispo";
+	this.usedBy = null;
+};
+
+Nombre.prototype.supprime = function(){
+	if(this.usedBy){
+		this.usedBy.supprime();
+	}
+	if(this.parent1){
+		this.parent1.libre();
+	}
+	if(this.parent2){
+		this.parent2.libre();
+	}
+	this.refCalcul.parentNode.removeChild(this.refCalcul);
+	this.refDispo.parentNode.removeChild(this.refDispo);
+	listeNombre.splice(listeNombre.indexOf(this),1);
+};
+
+// création et affichage de la disponibilité des nombres
+function creationNombresDispo(nb){
+	var nombre = document.createElement("div");
+	nombre.textContent = nb;
+	nombresDispo.appendChild(nombre);
+	return nombre;
+}
